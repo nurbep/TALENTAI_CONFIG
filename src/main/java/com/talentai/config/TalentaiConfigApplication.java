0a6b2ddd161a1d7aa744bb35d48686e76694cece
nurbep <nuruzzaman.bepari123@gmail.com>
2025-11-12 14:20:38 -0600

    currency exchange service

---
+++ src/main/java/com/talentai/config/TalentaiConfigApplication.java
@@ -0,0 +1,13 @@
+package com.talentai.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentaiConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentaiConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentai/config/TalentaiConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentai.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentaiConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
